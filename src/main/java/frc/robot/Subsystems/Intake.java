cf33e86930e397ccac1279ce32cf6ad696875257
Kman3340 <kman3340@outlook.com>
2026-02-28 20:31:01 -0500

    Cleaned Up Intake.java
    removed pids and velocity based in favor of percent based

--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -2,12 +2,10 @@ package frc.robot.Subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Common.PIDValues;
 
 public class Intake extends SubsystemBase {
     public enum IntakeState{Idle,Forward, ForwardSlow, Backward}
@@ -15,20 +13,10 @@ public class Intake extends SubsystemBase {
     SparkMax intakeMotor;
     SparkMaxConfig motorConfig;
 
-    private double kP;
-    private double kI;
-    private double kD;
-
-
     public Intake(){
         intakeMotor = new SparkMax(IntakeConstants.IntakeMotorID, MotorType.kBrushless);
         motorConfig = new SparkMaxConfig();
         motorConfig.inverted(IntakeConstants.ReversedIntake);
-        setPids(IntakeConstants.IntakePID);
-        motorConfig.closedLoop
-            .p(kP)
-            .i(kI)
-            .d(kD);
         intakeMotor.configure(motorConfig, com.revrobotics.ResetMode.kNoResetSafeParameters, com.revrobotics.PersistMode.kNoPersistParameters);
     }
 
@@ -37,17 +25,16 @@ public class Intake extends SubsystemBase {
     public void periodic() {
         switch (state) {
             case Forward:
-                //intakeMotor.getClosedLoopController().setSetpoint(IntakeConstants.IntakeForwardVelocity, ControlType.kVelocity);
                 intakeMotor.set(1);
                 break;
             case Idle:
                 intakeMotor.set(0);
                 break;   
             case ForwardSlow:
-                intakeMotor.getClosedLoopController().setSetpoint(IntakeConstants.IntakeForwardSlowVelocity, ControlType.kVelocity);
+                intakeMotor.set(0.5);
                 break;
             case Backward:
-                intakeMotor.getClosedLoopController().setSetpoint(IntakeConstants.IntakeBackwardVelocity, ControlType.kVelocity);
+                intakeMotor.set(-0.5);
                 break;
         }
     }
@@ -65,22 +52,14 @@ public class Intake extends SubsystemBase {
     public void setState(IntakeState state){
         this.state = state;
     }
-
-    public void setPids(PIDValues pids){
-        this.kP = pids.getP();
-        this.kI = pids.getI();
-        this.kD = pids.getD();
-    }
-    
     
     public static final class IntakeConstants{
         //TODO find constants
         public static final int IntakeMotorID = 10;
         public static final boolean ReversedIntake = false;
-        public static final double IntakeForwardVelocity = 4000;
-        public static final double IntakeForwardSlowVelocity = 2000;
-        public static final double IntakeBackwardVelocity = -2000;
-        public static final PIDValues IntakePID = new PIDValues(0.001, 0, 0);
+        public static final double IntakeForwardSpeed = 1.0;
+        public static final double IntakeForwardSlowSpeed = 0.5;
+        public static final double IntakeBackwardSpeed = -0.5;
     }
 
 }
